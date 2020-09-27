8fae851474f9fe5951eed220e1bfd4a569438126
FabianMosquera <rfmq9530@gmail.com>
2020-09-27 16:20:51 +0000

    [Fix: semistandar style]

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
-    $('input[type=checkbox]').click(function () {
-      const myListName = [];
-      const myId = [];
-      $('input[type=checkbox]:checked').each(function () {
-        myListName.push($(this).attr('data-name'));
-        myId.push($(this).attr('data-id'));
-      });
-      if (myListName.length === 0) {
-        $('.amenities h4').html('&nbsp;');
-      } else {
-        $('.amenities h4').text(myListName.join(', '));
-      }
-      console.log(myId);
+  $('input[type=checkbox]').click(function () {
+    const myListName = [];
+    const myId = [];
+    $('input[type=checkbox]:checked').each(function () {
+      myListName.push($(this).attr('data-name'));
+      myId.push($(this).attr('data-id'));
     });
+    if (myListName.length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(myListName.join(', '));
+    }
+    console.log(myId);
+  });
 });
 
-$.ajax ({
-    url: 'http://0.0.0.0:5001/api/v1/status/',
-    type: 'GET',
-    datatype: 'json',
+$.ajax({
+  url: 'http://0.0.0.0:5001/api/v1/status/',
+  type: 'GET',
+  datatype: 'json',
 
-    success: function(json) {
-        $('#api-status').addClass('available');
-    },
+  success: function (json) {
+    $('#api-status').addClass('available');
+  },
 
-    error: function(xhr, status) {
-        console.log('error ' + status);
-    }
+  error: function (xhr, status) {
+    console.log('error ' + status);
+  },
 });
